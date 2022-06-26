3791467e2446c511062123f5c6e6208b0f8da6e5
Hanna <anzhelenyuk@gmail.com>
2022-06-26 15:10:28 +0200

    Add profile information to the header, refresh user info.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,47 @@
-import '../styles/globals.css'
+import { css, Global } from '@emotion/react';
+import { useCallback, useEffect, useState } from 'react';
+import Layout from '../components/Layout';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  const [user, setUser] = useState();
+
+  const refreshUserProfile = useCallback(async () => {
+    const profileResponse = await fetch('/api/profile');
+
+    const profileResponseBody = await profileResponse.json();
+
+    if (!('errors' in profileResponseBody)) {
+      setUser(profileResponseBody.user);
+    } else {
+      profileResponseBody.errors.forEach((error) => console.log(error.message));
+      setUser(undefined);
+    }
+  }, []);
+
+  useEffect(() => {
+    refreshUserProfile().catch(() => console.log('fetch api failed'));
+  }, [refreshUserProfile]);
+
+  return (
+    <>
+      <Global
+        styles={css`
+          html,
+          body {
+            padding: 0;
+            font-family: Inter, -apple-system, BlinkMacSystemFont, Segoe UI,
+              Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+              Helvetica Neue, sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}
+      />
+      <Layout user={user}>
+        <Component {...pageProps} refreshUserProfile={refreshUserProfile} />
+      </Layout>
+    </>
+  );
 }
-
-export default MyApp
