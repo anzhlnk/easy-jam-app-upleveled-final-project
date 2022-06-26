111f8618adda5da707796f7bdd8d0c96c868b2f0
Hanna <anzhelenyuk@gmail.com>
2022-06-26 14:48:37 +0200

    Create api route for getting profile information.

---
+++ pages/api/profile.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getUserByValidSessionToken } from '../../util/database';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === 'GET') {
+    // to  get the cookie from the request
+    const token = req.cookies.sessionToken;
+
+    if (!token) {
+      res
+        .status(400)
+        .json({ errors: [{ message: 'No session token passed' }] });
+      return;
+    }
+
+    // to  get the user from the token
+    const user = await getUserByValidSessionToken(token);
+
+    if (!user) {
+      res
+        .status(400)
+        .json({ errors: [{ message: 'Session token not valid' }] });
+      return;
+    }
+
+    // to return the user
+    res.status(200).json({ user: user });
+  } else {
+    res.status(405).json({ errors: [{ message: 'Method not allowed' }] });
+  }
+}
