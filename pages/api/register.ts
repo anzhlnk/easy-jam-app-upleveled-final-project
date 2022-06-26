7064d3aa5b6af33ad037e3ae282b7aa859827745
Hanna <anzhelenyuk@gmail.com>
2022-06-26 14:43:18 +0200

    Create Api Route for registration.

---
+++ pages/api/register.ts
@@ -0,0 +1,71 @@
+import crypto from 'node:crypto';
+import bcrypt from 'bcrypt';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { createCSRFSecret } from '../../util/auth';
+import { createSerializedRegisterSessionTokenCookie } from '../../util/cookies';
+import {
+  createSession,
+  createUser,
+  getUserByUsername,
+} from '../../util/database';
+
+export type RegisterResponseBody =
+  | {
+      errors: {
+        message: string;
+      }[];
+    }
+  | { user: { id: number; username: string } };
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<RegisterResponseBody>,
+) {
+  // check the method to be post
+  if (req.method === 'POST') {
+    if (
+      typeof req.body.username !== 'string' ||
+      typeof req.body.password !== 'string' ||
+      !req.body.username ||
+      !req.body.password
+    ) {
+      res
+        .status(400)
+        .json({ errors: [{ message: 'username or password not provided' }] });
+      return;
+    }
+
+    // further checks are to be added here
+
+    if (await getUserByUsername(req.body.username)) {
+      res.status(401).json({ errors: [{ message: 'username already taken' }] });
+      return;
+    }
+    // to hash the password
+    const passwordHash = await bcrypt.hash(req.body.password, 12);
+
+    // to create the user
+    const newUser = await createUser(req.body.username, passwordHash);
+
+    // to create a token
+    const token = crypto.randomBytes(80).toString('base64');
+
+    // to create a secret
+
+    const csrfSecret = createCSRFSecret();
+
+    const session = await createSession(token, newUser.id, csrfSecret);
+
+    const serializedCookie = await createSerializedRegisterSessionTokenCookie(
+      session.token,
+    );
+
+    res
+      .status(200)
+      // to create cookies
+      .setHeader('set-Cookie', serializedCookie)
+      .json({ user: { id: newUser.id, username: newUser.username } });
+  } else {
+    res.status(405).json({ errors: [{ message: 'method not allowed' }] });
+  }
+}
