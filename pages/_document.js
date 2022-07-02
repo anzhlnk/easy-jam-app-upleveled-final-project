2ab87f30a4b25d13673d0bb762b4ea977c2d5b05
Hanna <anzhelenyuk@gmail.com>
2022-07-02 21:57:29 +0200

    Adapt crossOrigin to camelCase.

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function MyDocument() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Michroma&display=swap"
           rel="stylesheet"
