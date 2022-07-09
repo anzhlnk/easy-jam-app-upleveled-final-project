eef1e9bd022ded4a03c1605e955998102bd47e4e
Hanna <anzhelenyuk@gmail.com>
2022-07-09 21:01:43 +0200

    Add fonts.

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,10 @@ export default function MyDocument() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Michroma&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Michroma&display=swap"
           rel="stylesheet"
         />
       </Head>
