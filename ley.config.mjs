43620bdd68272279f7d64b3bd7b9b044915708cb
Hanna <anzhelenyuk@gmail.com>
2022-07-17 18:54:05 +0200

    Update ley.config file for the deployment.

--- ley.config.mjs
+++ ley.config.mjs
@@ -1,4 +1,4 @@
-import setPostgresDefaultsOnHeroku from './util/setPostgresDefaultsOnHeroku';
+const setPostgresDefaultsOnHeroku = require('./util/setPostgresDefaultsOnHeroku');
 
 setPostgresDefaultsOnHeroku();
 
