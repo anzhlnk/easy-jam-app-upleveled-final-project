c1703e4903dd5f77dab36aba70f192b23a6f1bd6
Hanna <anzhelenyuk@gmail.com>
2022-07-17 18:43:31 +0200

    Set up project for deployment.

---
+++ ley.config.js
@@ -0,0 +1,11 @@
+import setPostgresDefaultsOnHeroku from './util/setPostgresDefaultsOnHeroku';
+
+setPostgresDefaultsOnHeroku();
+
+const options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.ssl = { rejectUnauthorized: false };
+}
+
+module.exports = options;
--- util/database.js
+++ util/database.js
@@ -1,14 +1,27 @@
 import camelcaseKeys from 'camelcase-keys';
 import { config } from 'dotenv-safe';
 import postgres from 'postgres';
+import setPostgresDefaultsOnHeroku from './setPostgresDefaultsOnHeroku';
 
+setPostgresDefaultsOnHeroku();
 config();
 
+// Connect only once to the database
+// https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
 function connectOneTimeToDatabase() {
-  if (!globalThis.postgresSqlClient) {
-    globalThis.postgresSqlClient = postgres();
+  let sql;
+
+  if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL) {
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
+  } else {
+    if (!globalThis.postgresSqlClient) {
+      globalThis.postgresSqlClient = postgres();
+    }
+    sql = globalThis.postgresSqlClient;
   }
-  const sql = globalThis.postgresSqlClient;
 
   return sql;
 }
