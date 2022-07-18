eeeaeb0b97b8ff07dde6f1d615a54688a91ffb5e
Hanna <anzhelenyuk@gmail.com>
2022-07-18 11:43:38 +0200

    Fix spelling mistake.

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function MyDocument() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossoOigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Michroma&display=swap"
           rel="stylesheet"
