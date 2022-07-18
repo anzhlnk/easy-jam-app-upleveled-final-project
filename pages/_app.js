b591f829168ad6bcdb0dae7cf774559139c1bbd7
Hanna <anzhelenyuk@gmail.com>
2022-07-18 12:38:39 +0200

    Add favicon.

--- pages/_app.js
+++ pages/_app.js
@@ -31,6 +31,9 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/icon-apple-touch.png" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </Head>
       <Global
         styles={css`
