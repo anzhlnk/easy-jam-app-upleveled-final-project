e062d24252bb986dccd14c5418ce2a2d3f5784d9
Hanna <anzhelenyuk@gmail.com>
2022-07-18 18:01:09 +0200

    Add https back to the link. For testing.

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,14 @@ export default function MyDocument() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="//fonts.googleapis.com" />
-        <link rel="preconnect" href="//fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          href="//fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Michroma&display=swap"
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Michroma&display=swap"
           rel="stylesheet"
         />
       </Head>
