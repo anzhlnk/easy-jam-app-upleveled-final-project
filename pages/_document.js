b8726cc5784f4479e17cd24d96575eaf2b7f50d0
Hanna <anzhelenyuk@gmail.com>
2022-07-18 12:00:56 +0200

    Remove unnecessary links and set crossOrigin to true.

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,11 @@ export default function MyDocument() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Michroma&display=swap"
           rel="stylesheet"
