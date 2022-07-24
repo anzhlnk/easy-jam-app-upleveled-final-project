f8d2df1c46d80f20da4ed0dde467136cec2452b5
Hanna <anzhelenyuk@gmail.com>
2022-07-24 17:39:19 +0200

    Fix the issue of deleting a user when there are conversations.

--- migrations/1657561986-create-table-messages.js
+++ migrations/1657561986-create-table-messages.js
@@ -2,7 +2,7 @@ exports.up = async (sql) => {
   await sql`
 		CREATE TABLE messages (
 			id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-			personal_data_id integer REFERENCES personal_data (id),
+			personal_data_id integer REFERENCES personal_data (id) ON DELETE CASCADE,
 			conversation_id integer REFERENCES conversations (id),
 			timestamp timestamp NOT NULL DEFAULT NOW(),
 			content varchar(600) NOT NULL
